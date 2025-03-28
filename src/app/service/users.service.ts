3f189bfcbfd78a4e2f6b3157ddfbd3c13b7b252f
Vedaant Sankhe <vedaant.sankhe@gmail.com>
2025-03-29 01:26:19 +0530

    added prod url

--- src/app/service/users.service.ts
+++ src/app/service/users.service.ts
@@ -7,20 +7,21 @@ import { Observable } from 'rxjs';
 })
 export class UsersService {
 
+
   constructor(private http: HttpClient) { }
 
   getData(): Observable<any> {
-    const url = "http://localhost:8080/examiner/all";
+    const url = "http://34.47.168.104:8080/examiner/all";
     return this.http.get(url);
   }
 
   SaveData(user: any): Observable<any> {
-    const url = "http://localhost:8080/examiner/create";
+    const url = "http://34.47.168.104:8080/examiner/create";
     return this.http.post(url, user, { responseType: 'text' });
   }
 
   deleteUserById(examinerID: number): Observable<any> {
-    const url = `http://localhost:8080/examiner/delete/${examinerID}`;
+    const url = `http://34.47.168.104:8080/examiner/delete/${examinerID}`;
     return this.http.delete(url,{ responseType: 'text' });
   }
 
